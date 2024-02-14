50171c64bafb39c511d4122b5b031cde4d812300
Ерлан Сакан <ekosha_98@mail.ru>
2024-02-14 23:19:49 +0600

    [add] add goods

---
+++ TaskProject/Mediatr/GoodMediatr/Query/GoodAddQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using KDS.Primitives.FluentResult;
+using MediatR;
+using TaskProject.Interfaces;
+
+namespace TaskProject.Mediatr.Query;
+
+public class GoodAddQueryHandler : IRequestHandler<GoodAddQuery, Result<bool>>
+{
+    private readonly IGoodAdd _good;
+    public GoodAddQueryHandler(IGoodAdd todoitems)
+    {
+        _good = todoitems;
+    }
+    public async Task<Result<bool>> Handle(GoodAddQuery request, CancellationToken cancellationToken)
+    {
+        var item = await _good.AddGoodItem(request);
+        return Result.Success(item);
+    }
+}
---
+++ TaskProject/Services/GoodAddServices.cs
@@ -0,0 +1,37 @@
+﻿using DataManager.Base;
+using DataManager.EF;
+using DataManager.Model;
+using Newtonsoft.Json;
+using TaskProject.Interfaces;
+using TaskProject.Mediatr.Query;
+
+namespace TestMediatorApi.Services;
+
+public class GoodAddServices : IGoodAdd
+{
+    public readonly CategoryContext _context;
+    public GoodAddServices(CategoryContext context) => _context = context;
+    public async Task<bool> AddGoodItem(GoodAddQuery value)
+    {
+        var fields = new List<FieldDescribe>();
+        var goodDeserialize = JsonConvert.DeserializeObject<CategoryList>(value.CategoryFields);       
+        foreach (var field in goodDeserialize!.CategoryFields)
+        {
+            fields.Add(new FieldDescribe
+            {
+                Description = field.CategoryDescription,
+                CategoryId = field.CategoryId
+            });
+        }
+        await _context.Good.AddAsync(new Good
+        {
+            Name = value.Name,
+            FieldDescribe = fields,
+            Decription = value.Description,
+            CategoryId = value.CategoryId,
+            Price = value.Price
+        });
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
