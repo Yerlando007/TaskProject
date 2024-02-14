b0e41ac8c20830b674a84396e7f8f8f3d4967016
Ерлан Сакан <ekosha_98@mail.ru>
2024-02-14 23:26:12 +0600

    [add] get all good

--- TaskProject/Controllers/GoodAddController.cs
+++ TaskProject/Controllers/GoodAddController.cs
@@ -1,5 +1,6 @@
 using DataManager.Base;
 using Microsoft.AspNetCore.Mvc;
+using TaskProject.Mediatr.Command;
 using TaskProject.Mediatr.Query;
 
 namespace TaskProject.Controllers;
@@ -12,6 +13,13 @@ public class GoodAddController : BaseController
     public async Task<IActionResult> AddGood([FromForm] AddGoodFormData value)
     {
         var result = await Sender.Send(new GoodAddQuery(value));
+        return Ok(result);
+    }
+
+    [HttpGet("GetAllGoods")]
+    public async Task<IActionResult> GetAllGoods()
+    {
+        var result = await Sender.Send(new GetAllGoodCommand());
         return Ok(result);
     }
 }
---
+++ TaskProject/Mediatr/GoodMediatr/Command/GetAllGoodCommand.cs
@@ -0,0 +1,10 @@
+﻿using DataManager.EF;
+using KDS.Primitives.FluentResult;
+using MediatR;
+
+namespace TaskProject.Mediatr.Command;
+
+public class GetAllGoodCommand : IRequest<Result<List<Good>>>
+{
+    public GetAllGoodCommand() {}
+}
